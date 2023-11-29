386fa5a338752dd70fb8feecd10d419fabb05ce7
mohd noman <mohdnoman2751@gmail.com>
2023-11-30 04:31:13 +0530

    added functionality to list component

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ export default function App() {
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
           />
         </Grid>
       </Grid>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,13 +2,13 @@ import react from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating  from '@material-ui/lab';
+import Rating  from '@material-ui/lab/Rating';
 
 import useStyles from './styles'
 
-const Map = ({setCoordinates, setBounds, coordinates }) => {
+const Map = ({setCoordinates, setBounds, coordinates, places }) => {
     const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width: 600px)')
+    const isDesktop = useMediaQuery('(min-width: 600px)')
 
 
     return (
@@ -26,10 +26,34 @@ const Map = ({setCoordinates, setBounds, coordinates }) => {
               }}
               onChildClick={''}
              >
-
-             </GoogleMapReact>
-          </div>
-    );
+                { places?.map((place, i) => (
+                 <div className={classes.markerContainer}
+                     lat={Number(place.latitude)}
+                     lng={Number(place.longitude)}
+                     key={i}
+                 >
+                  { !isDesktop ? (
+                  <LocationOnOutlinedIcon color='primary' fontSize='large'/>
+                  ) : ( 
+                    <Paper elevation={3} className={classes.paper}> 
+                       <Typography className={classes.typography} variant="subtitle2" gutterbottom>
+                         {place.name}
+                       </Typography>
+                       <img 
+                         className={classes.pointer}
+                         src={place.photo ? place.photo.images.large.url : 'https://www.jetsetter.com/uploads/sites/7/2018/04/N-8dtAdp.jpeg' }
+                         alt={place.name}
+                       />
+                       <Rating size='small' value={Number(place.rating)} readOnly/>
+                    </Paper>
+                    ) 
+                }
+                 </div>
+               ))
+        } 
+                </GoogleMapReact>
+        </div>
+    )
 }
 
 export default Map;
